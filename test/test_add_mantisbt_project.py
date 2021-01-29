a58ae3d9e176d004dcf8cc556e01e6129e6cc238
fox140 <malcev223@gmail.com>
2021-01-29 15:05:33 +0300

    Изменения. Реализованны проверки через SOAP/

--- test/test_add_mantisbt_project.py
+++ test/test_add_mantisbt_project.py
@@ -3,10 +3,10 @@ from model.project import Project
 
 def test_add_project(app, db):
     project = Project(name="New project", description="Project description", status="development", view_state="public")
-    old_projects_list = db.get_projects_list()
+    old_projects_list = app.soap.get_projects_list(username="administrator", password="root")
     app.session.login(username="administrator", password="root")
     app.project.create_project(project)
     app.session.logout()
-    new_projects_list = db.get_projects_list()
+    new_projects_list = app.soap.get_projects_list(username="administrator", password="root")
     old_projects_list.append(project)
     assert sorted(old_projects_list, key=Project.id_or_max) == sorted(new_projects_list, key=Project.id_or_max)
--- test/test_delete_mantisbt_project.py
+++ test/test_delete_mantisbt_project.py
@@ -7,10 +7,10 @@ def test_delete_project(app, db):
     if len(db.get_projects_list()) == 0:
         app.project.create(
             Project(name="Test project", description="Test description", status="development", view_state="public"))
-    old_projects_list = db.get_projects_list()
+    old_projects_list = app.soap.get_projects_list(username="administrator", password="root")
     project_for_delete = random.choice(old_projects_list)
     app.project.delete_project_by_name(project_for_delete.name)
     app.session.logout()
-    new_projects_list = db.get_projects_list()
+    new_projects_list = app.soap.get_projects_list(username="administrator", password="root")
     old_projects_list.remove(project_for_delete)
     assert old_projects_list == new_projects_list
