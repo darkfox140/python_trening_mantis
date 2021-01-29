2f252ee6dff72936269319277cd3e194cbdd29bf
fox140 <malcev223@gmail.com>
2021-01-29 15:08:15 +0300

    Изменнения в функциях для проверки через SOAP.

--- model/project.py
+++ model/project.py
@@ -11,10 +11,11 @@ class Project:
         self.view_state = view_state
 
     def __repr__(self):
-        return "%s:%s" % (self.id, self.name)
+        return "%s:%s,%s" % (self.id, self.name, self.description)
 
     def __eq__(self, other):
-        return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name
+        return (self.id is None or other.id is None or self.id == other.id) and self.name == other.name \
+               and self.description == other.description
 
     def id_or_max(self):
         if self.id:
